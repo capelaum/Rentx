fcf958e94621f957a7b67c41a3c08752d807ee11
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-12-28 13:50:12 -0300

    feat: Redis Rate Limiter

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,12 +1,13 @@
 import "reflect-metadata";
 import "express-async-errors";
 import cors from "cors";
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
 
 import "@shared/container";
 import upload from "@config/upload";
 import { AppError } from "@shared/errors/AppError";
+import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
 import createConnection from "@shared/infra/typeorm";
 
 import swaggerFile from "../../../swagger.json";
@@ -17,6 +18,8 @@ createConnection();
 const app = express();
 app.use(express.json());
 
+app.use(rateLimiter);
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
@@ -25,7 +28,7 @@ app.use("/car_images", express.static(`${upload.tmpFolder}/car_images`));
 app.use(cors());
 app.use(router);
 
-app.use((err: Error, req: Request, res: Response) => {
+app.use(async (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({ message: err.message });
   }
