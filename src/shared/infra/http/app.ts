c45780365c7f4e9e245f17272c261854746b6e9f
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-12-27 22:26:08 -0300

    add cors + fixes

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import "express-async-errors";
-import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import express, { Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
 
 import "@shared/container";
@@ -21,9 +22,10 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/car_images", express.static(`${upload.tmpFolder}/car_images`));
 
+app.use(cors());
 app.use(router);
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+app.use((err: Error, req: Request, res: Response) => {
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({ message: err.message });
   }
